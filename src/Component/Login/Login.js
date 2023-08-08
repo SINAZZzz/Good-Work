0ce581b73440ef71b51a5b5122d7dd9dabd70972
Sina Zinsaz <sinazinsaz1@gamil.com>
2023-08-08 14:23:11 +0330

    add responsive login

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -14,8 +14,7 @@ export default function Login() {
 
   const [loginUser, setLoginUser] = useState("98123456789");
   const [hidden, setHidden] = useState(false);
-  // state code
-  const [code, setCode] = useState();
+
   // state input code
   const [num1, setNum1] = useState("");
   const [num2, setNum2] = useState("");
@@ -70,18 +69,27 @@ export default function Login() {
   }
 
   return (
-    <div className="flex h-[100vh] overflow-y-hidden font-Dana ">
+    <div
+      className="h-[100vh] overflow-y-hidden font-Dana
+    lg:flex sm:flex sm:flex-col-reverse "
+    >
       <Toaster position="bottom-center" reverseOrder={false} />
-      <div className="w-[70%] z-20 static bg-slate-50 flex justify-center items-center rounded-l-[70px]">
+      <div
+        className="
+      lg:w-[70%] lg:h-full z-20 static 
+    bg-slate-50 lg:items-center lg:rounded-l-[70px] lg:flex lg:justify-center 
+      sm:flex sm:flex-col sm:justify-start sm:content-center sm:w-full sm:h-[45%]
+       sm:rounded-t-[25px]"
+      >
         <div className="absolute z-10">
           <form
             onSubmit={handleSubmit}
             className={`${
               hidden == true ? "hidden" : ""
             } flex flex-col flex-nowrap justify-center items-center
-            content-stretch `}
+            content-stretch sm:mr-7 sm:mt-10 lg:mt-0 lg:mr-0 `}
           >
-            <h1 className=" mb-10 text-[1.1rem]">
+            <h1 className="mb-10 text-[1.1rem]">
               برای ورود شماره تلفن همراه خود را وارد کنید
             </h1>
             <div className="form__group field">
@@ -116,9 +124,9 @@ export default function Login() {
             className={`${
               hidden == false ? "hidden" : ""
             } flex flex-col flex-nowrap justify-center  items-center
-            content-stretch `}
+            content-stretch sm:mr-7 sm:mt-10 lg:mt-0 lg:mr-0`}
           >
-            <h1 className=" mb-7 text-[1.4rem]">
+            <h1 className=" lg:mb-7 sm:mb-4 lg:text-[1.4rem] sm:text-[1.18rem]">
               کد چهار رقمی ارسال شده را وارد کنید
             </h1>
             <div className="form__group1 field">
@@ -126,7 +134,7 @@ export default function Login() {
                 type="input"
                 id="input1"
                 ref={ref}
-                className="form__field1 mr-5"
+                className="form__field1 lg:mr-5 sm:mr-4 lg:w-[4rem] sm:w-[3.4rem]"
                 maxLength={1}
                 value={num1}
                 onChange={handleChangeNum1}
@@ -136,7 +144,7 @@ export default function Login() {
                 type="input"
                 id="input2"
                 ref={ref2}
-                className="form__field1 mr-5"
+                className="form__field1 lg:mr-5 sm:mr-4 lg:w-[4rem] sm:w-[3.4rem]"
                 maxLength={1}
                 value={num2}
                 onChange={handleChangeNum2}
@@ -146,7 +154,7 @@ export default function Login() {
                 type="input"
                 id="input3"
                 ref={ref3}
-                className="form__field1 mr-5"
+                className="form__field1 lg:mr-5 sm:mr-4 lg:w-[4rem] sm:w-[3.4rem]"
                 maxLength={1}
                 value={num3}
                 onChange={handleChangeNum3}
@@ -155,7 +163,7 @@ export default function Login() {
               <input
                 type="input"
                 id="input4"
-                className="form__field1"
+                className="form__field1 lg:w-[4rem] sm:w-[3.4rem]"
                 maxLength={1}
                 ref={ref4}
                 value={num4}
@@ -164,7 +172,7 @@ export default function Login() {
               />
             </div>
             <button
-              className="w-[20rem] mt-8 
+              className="lg:w-[20rem] sm:w-[16.8rem] lg:mt-8 sm:mt-12 
              bg-black/20 text-black/50 duration-700 hover:bg-blue-700 
              hover:text-white py-2 px-4 rounded text-lg"
               ref={ref5}
@@ -178,9 +186,14 @@ export default function Login() {
         <img
           src={Logo}
           alt=""
-          className="z-0 absolute left-20 top-[18rem] w-[18rem]"
+          className="z-0 absolute lg:left-20 lg:top-[18rem] lg:w-[18rem] 
+          sm:w-[261px] sm:top-[103px] sm:left-[60px]"
         />
-        <img src={Image} alt="" className=" w-[33rem] h-[100vh] z-10" />
+        <img
+          src={Image}
+          alt=""
+          className="lg:w-[33rem] lg:h-[104.1vh] sm:w-[25rem] sm:h-[28rem] sm:-mt-[2rem] z-10"
+        />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,22 @@ module.exports = {
         Montserrat: "Dana",
       },
     },
+    screens: {
+      sm: "320px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [],
 };
